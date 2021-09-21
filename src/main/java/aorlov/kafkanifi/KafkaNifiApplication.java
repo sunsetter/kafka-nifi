371caf22b529e92c581aec56126a359156d57caf
Anton Orlov <antonorlov@ziftsolutions.com>
2021-09-22 00:45:02 +0300

    Initial commit

---
+++ src/main/java/aorlov/kafkanifi/KafkaNifiApplication.java
@@ -0,0 +1,13 @@
+package aorlov.kafkanifi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaNifiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaNifiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aorlov/kafkanifi/KafkaNifiApplicationTests.java
@@ -0,0 +1,13 @@
+package aorlov.kafkanifi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaNifiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
